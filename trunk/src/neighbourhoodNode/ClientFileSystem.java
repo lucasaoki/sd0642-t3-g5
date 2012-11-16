008a65509bcca4beb9aa1f469937ad2ba43d31ea
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@e62d20bf-c889-5b44-0442-1266973be8b9>
2012-11-16 16:16:45 +0000

    - Juncao - #1 -

--- trunk/src/neighbourhoodNode/ClientFileSystem.java
+++ trunk/src/neighbourhoodNode/ClientFileSystem.java
@@ -20,126 +20,126 @@ import utilitesFileSystem.PipeMensageUtilites;
 
 public class ClientFileSystem implements PipeMsgListener, Runnable {
 
-	private final File home;
-	private NetworkManager manager;
-	private PeerGroup peerGroup;
-	private JxtaBiDiPipe pipe;
-	private PipeAdvertisement centralNode;
+    private final File home;
+    private NetworkManager manager;
+    private PeerGroup peerGroup;
+    private JxtaBiDiPipe pipe;
+    private PipeAdvertisement centralNode;
+    private FileManager onlyRead;
+    private MsgFileSystem msgFileSystem;
+    private int function;
+    private boolean create = false;
+    private boolean delete = false;
+    private boolean move = false;
+    private boolean where = false;
 
-	private FileManager onlyRead;
-	private MsgFileSystem msgFileSystem;
+    public ClientFileSystem() throws IOException, PeerGroupException {
 
-	private int function;
-	private boolean create = false;
-	private boolean delete = false;
-	private boolean move = false;
-	private boolean where = false;
+        msgFileSystem = new MsgFileSystem();
 
-	public ClientFileSystem() throws IOException, PeerGroupException {
+        home = new File(new File(".cache"), "client");
 
-		msgFileSystem = new MsgFileSystem();
+        manager = new NetworkManager(NetworkManager.ConfigMode.EDGE,
+                "ClientFileSystem", home.toURI());
+        manager.startNetwork();
 
-		home = new File(new File(".cache"), "client");
+        boolean waitForRendezvous = Boolean.valueOf(System.getProperty(
+                "RDWAIT", "false"));
 
-		manager = new NetworkManager(NetworkManager.ConfigMode.EDGE,
-				"ClientFileSystem", home.toURI());
-		manager.startNetwork();
+        if (waitForRendezvous) {
+            manager.waitForRendezvousConnection(0);
+        }
 
-		boolean waitForRendezvous = Boolean.valueOf(System.getProperty(
-				"RDWAIT", "false"));
-		
-		if (waitForRendezvous)
-			manager.waitForRendezvousConnection(0);
+        peerGroup = manager.getNetPeerGroup();
 
-		peerGroup = manager.getNetPeerGroup();
+        centralNode = ServerFileSystem.getPipeAdvertisement();
+        pipe = new JxtaBiDiPipe(peerGroup, centralNode, 20000, this, true);
+    }
 
-		centralNode = ServerFileSystem.getPipeAdvertisement();
-		pipe = new JxtaBiDiPipe(peerGroup, centralNode, 20000, this, true);
-	}
+    public void sendMessageForServerFileSystem(Message msg) throws IOException {
+        pipe.sendMessage(msg);
+    }
 
-	public void sendMessageForServerFileSystem(Message msg) throws IOException {
-		pipe.sendMessage(msg);
-	}
+    @Override
+    public void pipeMsgEvent(PipeMsgEvent event) {
+        // TODO Auto-generated method stub
+        Message msg = event.getMessage();
 
-	@Override
-	public void pipeMsgEvent(PipeMsgEvent event) {
-		// TODO Auto-generated method stub
-		Message msg = event.getMessage();
+        function = msgFileSystem.functionFromMessage(msg);
+        String response, fileName;
 
-		function = msgFileSystem.functionFromMessage(msg);
-		String response, fileName;
+        switch (function) {
+            case 0:
+                response = msgFileSystem.getResponseFromMessage(msg);
+                if (response.equals(PipeMensageUtilites.okCreate)) {
+                    // cria o arquivo
+                    get_data('c', msgFileSystem.getFileNameFromMessage(msg), null);
 
-		switch (function) {
-		case 0:
-			response = msgFileSystem.getResponseFromMessage(msg);
-			if (response.equals(PipeMensageUtilites.okCreate)) {
-				// cria o arquivo
-				System.out.println("1 " + response + " " + function
-						+ " " + msgFileSystem.getFileNameFromMessage(msg));
-				create = true;
-			} else {
-				System.out.println("2 " + response + " " + function
-						+ " " + msgFileSystem.getFileNameFromMessage(msg));
-				// informa que nao criou o arquivo
+                    System.out.println("1 " + response + " " + function
+                            + " " + msgFileSystem.getFileNameFromMessage(msg));
+                    create = true;
+                } else {
+                    System.out.println("2 " + response + " " + function
+                            + " " + msgFileSystem.getFileNameFromMessage(msg));
+                    // informa que nao criou o arquivo
 
-			}
-		//	System.exit(0);
-			break;
-		case 1:
-			response = msgFileSystem.getResponseFromMessage(msg);
-			if (response.equals(PipeMensageUtilites.okRemove)) {
-				// deletou o arquivo
-				System.out.println("1 " + response + " " + function
-						+ " " + msgFileSystem.getFileNameFromMessage(msg));
-				delete = true;
-			} else {
-				// informa que nao deletou o arquivo
-				System.out.println("2 " + response + " " + function
-						+ " " + msgFileSystem.getFileNameFromMessage(msg));
-			}
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		}
-	}
+                }
+                //	System.exit(0);
+                break;
+            case 1:
+                response = msgFileSystem.getResponseFromMessage(msg);
+                if (response.equals(PipeMensageUtilites.okRemove)) {
+                    // deletou o arquivo
+                    System.out.println("1 " + response + " " + function
+                            + " " + msgFileSystem.getFileNameFromMessage(msg));
+                    delete = true;
+                } else {
+                    // informa que nao deletou o arquivo
+                    System.out.println("2 " + response + " " + function
+                            + " " + msgFileSystem.getFileNameFromMessage(msg));
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
 
-	}
+    public static void main(String args[]) throws PeerGroupException,
+            IOException, InterruptedException {
 
-	public static void main(String args[]) throws PeerGroupException,
-			IOException, InterruptedException {
+        ClientFileSystem c = new ClientFileSystem();
 
-		ClientFileSystem c = new ClientFileSystem();
+        String sender = "1";
+        String receiver = "-1";
+        String fileName1 = "jaca.txt";
+        String fileName2 = "teste.txt";
 
-		String sender = "1";
-		String receiver = "-1";
-		String fileName1 = "jaca.txt";
-		String fileName2 = "teste.txt";
-		
-		Message msg1 = new Message();
-		Message msg2 = new Message();
-		
-		MsgFileSystem.createMessageCentralNodeFileSystem(msg1, sender, receiver,
-				PipeMensageUtilites.create, fileName1, "");
+        Message msg1 = new Message();
+        Message msg2 = new Message();
 
-		MsgFileSystem.createMessageCentralNodeFileSystem(msg2, sender, receiver,
-				PipeMensageUtilites.create, fileName2, "");
-		
-		c.sendMessageForServerFileSystem(msg1);
-		
-		//Thread.sleep(5000);
-		c.sendMessageForServerFileSystem(msg2);
+        MsgFileSystem.createMessageCentralNodeFileSystem(msg1, sender, receiver,
+                PipeMensageUtilites.create, fileName1, "");
 
-		while (true);
+        MsgFileSystem.createMessageCentralNodeFileSystem(msg2, sender, receiver,
+                PipeMensageUtilites.create, fileName2, "");
 
-	}
+        c.sendMessageForServerFileSystem(msg1);
+
+        //Thread.sleep(5000);
+        c.sendMessageForServerFileSystem(msg2);
+
+        while (true);
+
+    }
 }
